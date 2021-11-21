97d1cbc8a90290cbc660e4781a94e8642975f13d
sherryzyh <sherryyh.z@gmail.com>
2021-11-20 23:45:09 -0500

    remove duplicate, again

--- LibFewSHot/core/data/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-from .dataloader import get_dataloader
--- LibFewSHot/core/model/backbone/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from .dropblock import DropBlock
-from .maml_module import convert_maml_module
-from .mtl_module import convert_mtl_module
